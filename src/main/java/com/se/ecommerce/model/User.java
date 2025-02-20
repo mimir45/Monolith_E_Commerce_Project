f451992301414e837e1af3dad29f780325a204c2
mimir45 <samir.alizade.2001@gmail.com>
2025-02-20 04:45:48 +0400

    first commit

---
+++ src/main/java/com/se/ecommerce/model/User.java
@@ -0,0 +1,17 @@
+package com.se.ecommerce.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+}
